533de7c962ec029d1664c930fed381b36d0376b8
Sami Boulkenafet <Devera.22j@gmail.com>
2022-02-25 15:37:03 +0100

    fix bugs

--- src/components/AboutUs/aboutUs.jsx
+++ src/components/AboutUs/aboutUs.jsx
@@ -6,36 +6,33 @@ import Person from "./person/person";
 export default function AboutUs() {
   return (
     <div className="aboutus-container">
-      {" "}
-      <Reusable
-        className="heading"
-        title={"About Us"}
-        subHeading={"Why Choose Us"}
-      /> 
-
+      <h4 className="aboutus-header">
+        We're a group of six enthiousiastic computer science engeneers with high
+        hopes and dreams, full of petenciel and very hard workers, we're here to
+        satisfy every need that our clients can hope for.
+      </h4>
       <div className="person-cards-container">
-
-      <Person
-        title="Boulkenafet Sami"
-        desc="Sometimes ou just want to suicide "
-      />
-      <Person
-        title="Boulkenafet Sami"
-        desc="Sometimes ou just want to suicide "
-      />
-      <Person
-        title="Boulkenafet Sami"
-        desc="Sometimes ou just want to suicide "
-      />
-      <Person
-        title="Boulkenafet Sami"
-        desc="Sometimes ou just want to suicide "
-      />
-      <Person
-        title="Boulkenafet Sami"
-        desc="Sometimes ou just want to suicide "
-      />
-    </div>
+        <Person
+          title="Boulkenafet Sami"
+          desc="Sometimes ou just want to suicide "
+        />
+        <Person
+          title="Boulkenafet Sami"
+          desc="Sometimes ou just want to suicide "
+        />
+        <Person
+          title="Boulkenafet Sami"
+          desc="Sometimes ou just want to suicide "
+        />
+        <Person
+          title="Boulkenafet Sami"
+          desc="Sometimes ou just want to suicide "
+        />
+        <Person
+          title="Boulkenafet Sami"
+          desc="Sometimes ou just want to suicide "
+        />
       </div>
+    </div>
   );
 }
--- src/components/AboutUs/person/person.jsx
+++ src/components/AboutUs/person/person.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import "./person.css";
 
 export default function Person(props) {
+  const { title, desc } = props;
 
-    const {title, desc} = props
-
-    return (
+  return (
     <section className="person-cards">
       <div className="person-card-shape">
-        <div className="person-icon"></div>
+        <div className="header-info">
+          <div className="person-icon">
+            <span className="full-name">{title}</span>
+          </div>
+        </div>
         <div className="person-info">
-          <h3>{title}</h3>
           <p>{desc}</p>
         </div>
       </div>
