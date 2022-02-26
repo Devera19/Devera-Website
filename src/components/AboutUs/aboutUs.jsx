44c5312e99c0311a5495581fd47a7a8d2dc22152
Sami Boulkenafet <Devera.22j@gmail.com>
2022-02-26 02:30:36 +0100

    add aboutUs

--- src/components/AboutUs/aboutUs.jsx
+++ src/components/AboutUs/aboutUs.jsx
@@ -1,36 +1,57 @@
 import React from "react";
-import Reusable from "../utils/reusable/reusable";
 import "./aboutUs.css";
 import Person from "./person/person";
+import madara from './images/madara.jpg'
+import itachi from './images/itachi.jpg'
+import obito from './images/obito.jpg'
+import deidara from './images/deidara.jpg'
+import kakuzu from './images/kakuzu.jpg'
+import pain from './images/pain.jpg'
 
 export default function AboutUs() {
   return (
     <div className="aboutus-container">
       <h4 className="aboutus-header">
-        We're a group of six enthiousiastic computer science engeneers with high
-        hopes and dreams, full of petenciel and very hard workers, we're here to
+        We're a group of six enthusiastic computer science engineers with high
+        hopes and dreams, full of potencial and very hard workers, we're here to
         satisfy every need that our clients can hope for.
       </h4>
       <div className="person-cards-container">
         <Person
           title="Boulkenafet Sami"
-          desc="Sometimes ou just want to suicide "
+          desc="Front End and Desktop Developer "
+          languages="English, French, Arabic."
+          background={madara}
         />
         <Person
-          title="Boulkenafet Sami"
-          desc="Sometimes ou just want to suicide "
+          title="Zahreddine Soualem"
+          desc="Backend and Logo Designer "
+          languages="English, Arabic."
+          background={itachi}
         />
         <Person
-          title="Boulkenafet Sami"
-          desc="Sometimes ou just want to suicide "
+          title="Akram Akram"
+          desc="Front End Developer "
+          languages="English, French, Arabic."
+          background={obito}
         />
         <Person
-          title="Boulkenafet Sami"
-          desc="Sometimes ou just want to suicide "
+          title="Khaled Benbouabdallah"
+          desc="Back End Developer "
+          languages="English, French, Arabic."
+          background={deidara}
         />
         <Person
-          title="Boulkenafet Sami"
-          desc="Sometimes ou just want to suicide "
+          title="Mohamed Mohamed"
+          desc="UI & UX Designer  "
+          languages="English, French, Arabic."
+          background={kakuzu}
+        />
+        <Person
+          title="Islem Islem"
+          desc="Mobile Application developer "
+          languages="English, French, Arabic."
+          background={pain}
         />
       </div>
     </div>
--- src/components/AboutUs/person/person.jsx
+++ src/components/AboutUs/person/person.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import "./person.css";
 
+
 export default function Person(props) {
-  const { title, desc } = props;
+  const { background, languages, title, desc } = props;
 
   return (
     <section className="person-cards">
       <div className="person-card-shape">
         <div className="header-info">
           <div className="person-icon">
-            <span className="full-name">{title}</span>
+          <img src={background} alt="logo" />
           </div>
+          <span className="full-name">{title}</span>
         </div>
         <div className="person-info">
           <p>{desc}</p>
         </div>
+        <div className="more-info">
+        <i class="fa-solid fa-globe"></i>
+         <span>{languages}</span>
+        </div>
+
+        <div className="contact-staff">
+
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-github"></i>
+        </div>
+
+          
       </div>
     </section>
   );
