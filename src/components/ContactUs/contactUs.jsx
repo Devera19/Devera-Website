45da164627494b860e89e1a6fd4e533d7538a05e
Sami Boulkenafet <Devera.22j@gmail.com>
2022-02-27 01:41:38 +0100

    fix bugs

--- src/components/ContactUs/contactUs.jsx
+++ src/components/ContactUs/contactUs.jsx
@@ -1,16 +1,47 @@
 import React from "react";
-import "./contactUs.css"
+import "./contactUs.css";
 import Reusable from "../utils/reusable/reusable";
+import social from './social.jpeg'
 
 export default function ContactUs() {
   return (
     <div className="contactus-container">
       <div className="contactus-header">
-        <Reusable title="Contact us" subHeading="Happy to see your feedback" />
+        <Reusable title="Contact us" subHeading="Let's keep in touch" />
       </div>
 
       <div className="contactus-body">
+        <div className="contactus-left">
+          <h2> Get in touch 🤝</h2>
 
+          <div className="second-icons">
+            <a href="https://www.facebook.com/profile.php?id=100015419709090">
+              <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="">
+              <i class="fa-brands fa-google-plus"></i>
+            </a>
+            <a href="">
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="">
+              <i class="fa-brands fa-youtube"></i>
+            </a>
+            <a href="">
+              <i class="fa-brands fa-twitter"></i>
+            </a>
+          </div>
+
+          <div className="send-message">
+            <span className="send-your-message">Send your message !</span>
+            <img src={social} alt="" />
+          </div>
+        </div>
+        <div className="contactus-right">
+          <span> Name </span>
+          <span> Email </span>
+          <span> Message </span>
+        </div>
       </div>
     </div>
   );
